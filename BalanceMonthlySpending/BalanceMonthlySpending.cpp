9dfc1d07ec66297cb9ab69952eb43edf327bc27e
leer89 <leer89@gmail.com>
2016-12-30 10:46:44 -0500

    v1 - creates array with 10 elements and tallies user's expenses and checks to see if user saved at least 25% of paycheck after paying monthly expenses

--- BalanceMonthlySpending/BalanceMonthlySpending.cpp
+++ BalanceMonthlySpending/BalanceMonthlySpending.cpp
@@ -6,6 +6,45 @@
 
 int main()
 {
-    return 0;
+	using namespace std;
+
+	// me = monthly expenditures, mpc = monthly paycheck, spct = saving percentage
+	// sreq = savings required
+	int i, j, expenses[10], me, mpc, spct, sreq, monthEnd;
+
+	cout << "Monthly paycheck: " << endl;
+	cout << "Savings Percentage Goal: ";
+	cin >> mpc;
+	cin >> spct;
+
+	do {
+		cout << "Enter up to 10 expenses: \n";
+		cin >> expenses[0];
+		
+		// set expenses into array
+		for (i = 1; i < 10; i++)
+		{
+			cin >> expenses[i];
+		}
+
+		// tally expenses
+		for (j = 0; j < 10; j++) {
+			me = 0;
+			me += expenses[j];
+		}
+	} while (me >= mpc);
+
+	cout << "Monthly Expenses: " << me << endl;
+	cout << "Monthly Paycheck: " << mpc << endl;
+
+	// savings required = monthly paycheck * savings percentage / 100
+	sreq = mpc * (spct / 100);
+	// month end = monthly paycheck - monthly expenses
+	monthEnd = mpc - me;
+
+	cout << "Saving Percentage Goal: " << spct << endl;
+	cout << "Savings Required: " << sreq << endl;
+
+	return 0;
 }
 
